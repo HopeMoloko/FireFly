113839bf3c68dbe0d84cf4ac4b76efc10415f9b7
HopeProject1 <hopemmoloko@gmail.com>
2019-03-04 12:02:59 +0200

    Updated MCMC with unit Tests

--- Nested_Sampling.2.0/Nested-Sampling_Simple_Tests/Metropolis_Hasting_3_0.py
+++ Nested_Sampling.2.0/Nested-Sampling_Simple_Tests/Metropolis_Hasting_3_0.py
@@ -44,9 +44,9 @@ def MH_acceptance(loglikelihood_new,Prior_new,loglikelihood_old,Prior_old):
 
     #Acceptance ratio
 
-    logR = (loglikelihood_new - loglikelihood_old)*(Prior_new/Prior_old)
+    logR = (loglikelihood_new - loglikelihood_old)
 
-    R = np.exp(logR)
+    R = np.exp(logR)*(Prior_new/Prior_old)
 
     #Generate a uniform random Number
     u = np.random.uniform()
@@ -118,10 +118,23 @@ def test_accept_worse_sample_mh_acceptance(): #sometimes
     #these values should yield R = 0.65 which is greater than u but smaller than 1 and thus we can accept the worse sample at random
 
     assert (MH_acceptance(loglikelihood_new,Prior_new,loglikelihood_old,Prior_old) == 1.0)
+
+def test_literal_types_int_mh_acceptance():
+    #inputs (logLikelihood ints not floats)
+    np.random.seed(1) #will yeild u = 0.41
+    loglikelihood_new = -9
+    Prior_new         = 0.5  #in the bounds but its the worst sample
+
+    loglikelihood_old = -6
+    Prior_old         = 0.5
+
+    #these values will yield R = 0.049 , so they are smaller than 1 and u, therefore the sample must be rejected
+
+    assert (MH_acceptance(loglikelihood_new,Prior_new,loglikelihood_old,Prior_old) == 0.0)
 #########################################################################################################
 
 
-#####################          Main mcmc     ###################################################################################################
+#####################          Main mcmc     ##########################################################
 
 def MH_mcmc(loglikelihood_func,Prior_func,theta,args_loglike,args_prior,mcmc_steps,stepsize,Nested_Sampling=False,Nested_Sampling_args=None):
     """ Metropolis Hasting MCMC Algorithm
